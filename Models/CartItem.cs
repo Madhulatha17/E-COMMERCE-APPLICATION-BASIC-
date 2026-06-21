1d9307b4b035f3d3e9dced281edc2a4c5ac8b73e
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-21 22:23:20 +0530

    Create CartItem.cs

---
+++ Models/CartItem.cs
@@ -0,0 +1,13 @@
+namespace ECommerceApp.Models
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
